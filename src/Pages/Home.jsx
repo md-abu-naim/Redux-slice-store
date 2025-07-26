3f25216bff4d0e7ebe877934b4220bcaf6986943
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-26 21:29:57 +0600

    home page format

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import Counter from "../Components/Counter";
 const Home = () => {
     return (
         <div>
-            Home Page
+            <h1 className="bg-green-400 p-5 text-3xl font-extrabold text-white mb-5">Home Page</h1>
             <Counter />
         </div>
     );

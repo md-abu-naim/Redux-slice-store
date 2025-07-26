ce99e296e3db1a59f9ef3be827a6486690361c14
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-26 21:16:43 +0600

    set count is daynamic

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,15 +1,16 @@
 import { useSelector } from "react-redux";
 
 const Counter = () => {
-    // const {count} = useSelector(state.Counter)
-    // return (
-    //     <div>
-    //         <button className="bg-gray-600 p-2 font-semibold text-white">Increment by 5</button>
-    //         <button className="bg-gray-800 p-2 font-semibold text-white">Increment</button>
-    //         <h2>0</h2>
-    //         <button className="bg-gray-600 p-2 font-semibold text-white">Decrement</button>
-    //         <button className="bg-gray-800 p-2 font-semibold text-white">Decrement by 5</button>
-    //     </div>
+    const {count} = useSelector(state => state.counter)
+    
+    return (
+        <div>
+            <button className="bg-gray-600 p-2 font-semibold text-white">Increment by 5</button>
+            <button className="bg-gray-800 p-2 font-semibold text-white">Increment</button>
+            <h2>{count}</h2>
+            <button className="bg-gray-600 p-2 font-semibold text-white">Decrement</button>
+            <button className="bg-gray-800 p-2 font-semibold text-white">Decrement by 5</button>
+        </div>
     );
 };
 

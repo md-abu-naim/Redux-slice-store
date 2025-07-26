c67c6fd05c7a73a9e1a0a5e47191298a2e6998f4
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-26 20:56:53 +0600

    counter page connect with home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
+import Counter from "../Components/Counter";
 
 const Home = () => {
     return (
         <div>
             Home Page
-            
+            <Counter />
         </div>
     );
 };

4ef12ea1d8e9c30520e049139b107b4e5a7cdf14
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-26 21:26:35 +0600

    incrementByValue function successfully working

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment, incrementByValue } from "../Redux/Features/Counter/CounterSlice";
+import { decrement, decrementByValue, increment, incrementByValue } from "../Redux/Features/Counter/CounterSlice";
 
 const Counter = () => {
     const {count} = useSelector(state => state.counter)
@@ -10,7 +10,7 @@ const Counter = () => {
             <button onClick={() => dispatch(increment())} className="bg-gray-800 p-2 font-semibold text-white">Increment</button>
             <h2>{count}</h2>
             <button onClick={() => dispatch(decrement())} className="bg-gray-600 p-2 font-semibold text-white">Decrement</button>
-            <button className="bg-gray-800 p-2 font-semibold text-white">Decrement by 5</button>
+            <button onClick={() => dispatch(decrementByValue(5))} className="bg-gray-800 p-2 font-semibold text-white">Decrement by 5</button>
         </div>
     );
 };

68429baf076ad821bc70ab34d75fcdaf939a64fe
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-26 21:21:19 +0600

    ecrement functionality sucsessfully working

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { increment } from "../Redux/Features/Counter/CounterSlice";
+import { decrement, increment } from "../Redux/Features/Counter/CounterSlice";
 
 const Counter = () => {
     const {count} = useSelector(state => state.counter)
@@ -9,7 +9,7 @@ const Counter = () => {
             <button className="bg-gray-600 p-2 font-semibold text-white">Increment by 5</button>
             <button onClick={() => dispatch(increment())} className="bg-gray-800 p-2 font-semibold text-white">Increment</button>
             <h2>{count}</h2>
-            <button className="bg-gray-600 p-2 font-semibold text-white">Decrement</button>
+            <button onClick={() => dispatch(decrement())} className="bg-gray-600 p-2 font-semibold text-white">Decrement</button>
             <button className="bg-gray-800 p-2 font-semibold text-white">Decrement by 5</button>
         </div>
     );

e0b2305e616028a6b555790ef3637e2c26e85e41
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-26 21:14:39 +0600

    connect slice with store

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,13 +1,15 @@
+import { useSelector } from "react-redux";
 
 const Counter = () => {
-    return (
-        <div>
-            <button className="bg-gray-600 p-2 font-semibold text-white">Increment by 5</button>
-            <button className="bg-gray-800 p-2 font-semibold text-white">Increment</button>
-            <h2>0</h2>
-            <button className="bg-gray-600 p-2 font-semibold text-white">Decrement</button>
-            <button className="bg-gray-800 p-2 font-semibold text-white">Decrement by 5</button>
-        </div>
+    // const {count} = useSelector(state.Counter)
+    // return (
+    //     <div>
+    //         <button className="bg-gray-600 p-2 font-semibold text-white">Increment by 5</button>
+    //         <button className="bg-gray-800 p-2 font-semibold text-white">Increment</button>
+    //         <h2>0</h2>
+    //         <button className="bg-gray-600 p-2 font-semibold text-white">Decrement</button>
+    //         <button className="bg-gray-800 p-2 font-semibold text-white">Decrement by 5</button>
+    //     </div>
     );
 };
 
--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import counterReducer from './Features/Counter/CounterSlice'
 
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        counter: counterReducer
+    }
 })
 
 export default store;

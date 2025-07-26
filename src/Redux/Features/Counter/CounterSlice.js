9378ee68c0f278d91a19f1d2c4797806ea8640ef
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-26 21:09:57 +0600

    increment functionality

--- src/Redux/Features/Counter/CounterSlice.js
+++ src/Redux/Features/Counter/CounterSlice.js
@@ -8,8 +8,12 @@ const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
-
+        increment: state => {
+            state.count = state.count + 1;
+        }
     }
 })
+
+export const {increment} = counterSlice.actions;
 
 export default counterSlice.reducer

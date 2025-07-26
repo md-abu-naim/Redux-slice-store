a18749b8b957b77c458d52b04309e1e500c2f231
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-26 21:11:44 +0600

    create increment & decrement button

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -3,6 +3,10 @@ const Counter = () => {
     return (
         <div>
             <button className="bg-gray-600 p-2 font-semibold text-white">Increment by 5</button>
+            <button className="bg-gray-800 p-2 font-semibold text-white">Increment</button>
+            <h2>0</h2>
+            <button className="bg-gray-600 p-2 font-semibold text-white">Decrement</button>
+            <button className="bg-gray-800 p-2 font-semibold text-white">Decrement by 5</button>
         </div>
     );
 };

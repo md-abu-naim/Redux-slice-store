5effb68a6e3df8da2fdad518c7754a1a3f037693
abunaim34 <farhanadnanfarabi@gmail.com>
2025-07-26 21:18:56 +0600

    increment function successfully working

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,12 +1,13 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { increment } from "../Redux/Features/Counter/CounterSlice";
 
 const Counter = () => {
     const {count} = useSelector(state => state.counter)
-    
+    const dispatch = useDispatch()
     return (
         <div>
             <button className="bg-gray-600 p-2 font-semibold text-white">Increment by 5</button>
-            <button className="bg-gray-800 p-2 font-semibold text-white">Increment</button>
+            <button onClick={() => dispatch(increment())} className="bg-gray-800 p-2 font-semibold text-white">Increment</button>
             <h2>{count}</h2>
             <button className="bg-gray-600 p-2 font-semibold text-white">Decrement</button>
             <button className="bg-gray-800 p-2 font-semibold text-white">Decrement by 5</button>
